35e1be1aa82b224960a2d0caf873170baeec114b
EmilyPardo <134694623+EmilyPardo@users.noreply.github.com>
2023-06-26 11:20:26 +0300

    Add files via upload

---
+++ LineCounter.py
@@ -0,0 +1,9 @@
+file = r'D:\Doctorantura\DimSerbezov\allf.eigenstratgeno'
+fileout = r'D:\Doctorantura\DimSerbezov\wholefile\output.csv'
+
+i = 0
+f = open(fileout, "r")
+for i, line in enumerate(f):
+      pass
+print(i + 1)
+f.close()
